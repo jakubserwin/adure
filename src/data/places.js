bcf4c83068fb5733e2066aee33fef488a2009921
Jakub Serwin <kuba.serwin99@gmail.com>
2021-08-19 12:25:00 +0200

    Places

---
+++ src/data/places.js
@@ -0,0 +1,111 @@
+export default [
+  {
+    id: 1,
+    name: 'Kalaupapa cliffs Hawaii',
+    label: `Molokai's famous sea cliffs are responsible for this and are among the highest in the world at three thousand feet above sea level.`,
+    label:
+      'One of the most remote locations in Hawaii due to unique volcanic and geologic activity over millions of years.',
+    img: 'hawaii',
+    color: '#9F3804',
+    slider: [
+      {
+        id: '1a',
+        img: 'cliff-1',
+      },
+      {
+        id: '1b',
+        img: 'cliff-2',
+      },
+      {
+        id: '1c',
+        img: 'cliff-3',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Perito Moreno Glacier',
+    label:
+      'One of the major tourist attractions in southern Patagonia and the rupture of this glacier is considered one of the most impressive natural spectacles in the world.',
+    img: 'glacier',
+    color: '#5191B8',
+    slider: [
+      {
+        id: '2a',
+        img: 'iced-1',
+      },
+      {
+        id: '2b',
+        img: 'iced-2',
+      },
+      {
+        id: '2c',
+        img: 'iced-3',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Banff National Park',
+    label:
+      'Rocky Mountain peaks, turquoise glacial lakes, a picture-perfect mountain town and village, abundant wildlife and scenic drives come together in Canada’s first national park',
+    img: 'park',
+    color: '#539987',
+    slider: [
+      {
+        id: '3a',
+        img: 'park-1',
+      },
+      {
+        id: '3b',
+        img: 'park-2',
+      },
+      {
+        id: '3c',
+        img: 'park-3',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Cultural capital of Japan',
+    label: `Kyoto is home to roughly one quarter of Japan's national treasures, countless shrines and temples, and seventeen sites recognized by UNESCO as World Heritage Sites.`,
+    img: 'japan',
+    color: '#C5445E',
+    slider: [
+      {
+        id: '4a',
+        img: 'kyoto-1',
+      },
+      {
+        id: '4b',
+        img: 'kyoto-2',
+      },
+      {
+        id: '4c',
+        img: 'kyoto-3',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: `Luxor's Karnak Temple`,
+    label: `The largest religious building ever constructed. The temple of Karnak was known as Ipet-isu—or “most select of places”—by the ancient Egyptians. It is a city of temples built over 2,000 years.`,
+    img: 'karnak',
+    color: '#FBB84B',
+    slider: [
+      {
+        id: '5a',
+        img: 'temple-1',
+      },
+      {
+        id: '5b',
+        img: 'temple-2',
+      },
+      {
+        id: '5c',
+        img: 'temple-3',
+      },
+    ],
+  },
+];
