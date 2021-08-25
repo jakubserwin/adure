cafc5ea1bd3e2962de8c77413a68fcc1ca9fd0c5
Jakub Serwin <kuba.serwin99@gmail.com>
2021-08-25 17:26:54 +0200

    Card component

---
+++ src/components/UI/VCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <component :is="cardData.icon" />
+    <p class="card__title">{{ cardData.title }}</p>
+    <p class="card__desc">{{ cardData.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardData: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  box-shadow: rgba(149, 157, 165, 0.15) 0px 5px 20px;
+  background: #fff;
+  padding: 1.5rem;
+
+  display: grid;
+  gap: 0.5rem;
+  place-items: center;
+  border-radius: 1rem;
+  text-align: center;
+
+  &__title {
+    font: 500 1.6rem 'Roboto', sans-serif;
+    color: var(--color-dark);
+  }
+
+  &__desc {
+    font: 300 1.3rem 'Nunito', sans-serif;
+    color: #262626;
+  }
+
+  svg {
+    color: var(--color-primary);
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
---
+++ src/data/cards.js
@@ -0,0 +1,31 @@
+// prettier-ignore
+import {
+  HeartOutline, MapMarkerOutline, TreeOutline, StarOutline,
+} from 'mdue';
+
+export default [
+  {
+    id: 1,
+    icon: HeartOutline,
+    title: 'fit',
+    desc: 'Lorem ipsum dolor sit amet, consectetur',
+  },
+  {
+    id: 2,
+    icon: MapMarkerOutline,
+    title: 'guidance',
+    desc: 'Lorem ipsum dolor sit amet, consectetur',
+  },
+  {
+    id: 3,
+    icon: TreeOutline,
+    title: 'wild nature',
+    desc: 'Lorem ipsum dolor sit amet, consectetur',
+  },
+  {
+    id: 4,
+    icon: StarOutline,
+    title: 'hight rated',
+    desc: 'Lorem ipsum dolor sit amet, consectetur',
+  },
+];
